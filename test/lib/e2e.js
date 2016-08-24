7dcf221e76ae23119105bb624e9462e30757cb71
Adam Terlson <adam.terlson@internations.org>
2016-08-24 19:11:46 +0200

    Update all the tests to the new syntax

--- test/lib/e2e.js
+++ test/lib/e2e.js
@@ -8,17 +8,26 @@ import { createStore, combineReducers } from 'redux';
 import assert from 'assert';
 import { bbDispatch, bbCreateStore } from '../../lib';
 
+const incrementingModelReducer = (model, action) => {
+    if (action.type === 'INCREMENT') {
+        model.set('num', model.get('num') - 1);
+    }
+    return model;
+};
+const growingCollectionReducer = (collection, action) => {
+    if (action.type === 'PUSH') {
+        this.push(action.payload);
+    }
+    return collection;
+};
 const IncrementingModel = Backbone.Model.extend({
     initialize() {
-        this.listenTo(this, 'INCREMENT', () =>
-            this.set('num', this.get('num') + 1));
         this.listenTo(this, 'DECREMENT', () =>
             this.set('num', this.get('num') - 1));
     },
 });
 const GrowingCollection = Backbone.Collection.extend({
     initialize() {
-        this.listenTo(this, 'PUSH', (obj) => this.push(obj));
         this.listenTo(this, 'POP', (obj) => this.pop(obj));
     },
 });
@@ -29,11 +38,34 @@ describe('e2e communication', () => {
     let growingCollection;
     let store;
 
-    describe('bbCreateStore()(model)', () => {
+    describe('error scenarios', () => {
+        it('throws if reducer not passed', () => {
+            assert.throws(
+                () => bbCreateStore()(),
+                'bbCreateStore()(reducer, bbEntity) - Must define a reducer.'
+            );
+        });
+
+        it('throws if bbEntity not passed', () => {
+            assert.throws(
+                () => bbCreateStore()(),
+                'bbCreateStore()(reducer, bbEntity) - Must define a bbEntity.'
+            );
+        });
+
+        it('throws if reducer returns undefined', () => {
+            assert.throws(
+                () => bbCreateStore()(() => undefined),
+                'Reducer must have a return value'
+            );
+        });
+    });
+
+    describe('bbCreateStore(createStore)(incrementingModelReducer, model)', () => {
         beforeEach(() => {
             listenerCalledCount = 0;
             incrementingModel = new IncrementingModel({ num: 3 });
-            store = bbCreateStore()(incrementingModel);
+            store = bbCreateStore(createStore)(incrementingModelReducer, incrementingModel);
             store.subscribe(() => listenerCalledCount++);
         });
 
@@ -58,12 +90,72 @@ describe('e2e communication', () => {
         });
     });
 
-    describe('bbCreateStore()({ model, collection })', () => {
+    describe('bbCreateStore()(incrementingModelReducer, model)', () => {
         beforeEach(() => {
             listenerCalledCount = 0;
             incrementingModel = new IncrementingModel({ num: 3 });
+            store = bbCreateStore()(incrementingModelReducer, incrementingModel);
+            store.subscribe(() => listenerCalledCount++);
+        });
+
+        describe('backbone -> store', () => {
+            it('should affect store change via bbDispatch', () => {
+                bbDispatch(incrementingModel, 'INCREMENT');
+
+                assert.equal(incrementingModel.get('num'), 4);
+                assert.equal(store.getState().num, 4);
+                assert.equal(listenerCalledCount, 1);
+            });
+        });
+
+        describe('store -> backbone', () => {
+            it('should affect model change via store.dispatch', () => {
+                store.dispatch({ type: 'INCREMENT' });
+
+                assert.equal(incrementingModel.get('num'), 4);
+                assert.equal(store.getState().num, 4);
+                assert.equal(listenerCalledCount, 1);
+            });
+        });
+
+        it('should be called with the previous state in the next reducer call', () => {
+            const prevStateArgs = [];
+            const defaultState = { defaultStateArg: true };
+
+            store = bbCreateStore()((state, { type }) => {
+                prevStateArgs.push(state);
+
+                switch (type) {
+                case 'INCREMENT':
+                    return 'incremented';
+                case 'DECREMENT':
+                    return 'decremented';
+                default:
+                    return state;
+                }
+            }, defaultState);
+
+            store.dispatch({ type: 'INCREMENT' });
+            store.dispatch({ type: 'DECREMENT' });
+            store.dispatch({ type: 'Just push it' });
+
+            assert.deepEqual(prevStateArgs[0], defaultState);
+            assert.deepEqual(prevStateArgs[1], defaultState);
+            assert.equal(prevStateArgs[2], 'incremented');
+            assert.equal(prevStateArgs[3], 'decremented');
+        });
+    });
+
+    describe('bbCreateStore()(combineReducers({ modelReducer, collectionReducer }), { model, collection })', () => {
+        beforeEach(() => {
+            const reducer = combineReducers({
+                model: incrementingModelReducer,
+                collection: growingCollectionReducer,
+            });
+            listenerCalledCount = 0;
+            incrementingModel = new IncrementingModel({ num: 3 });
             growingCollection = new GrowingCollection([{}, {}, {}]);
-            store = bbCreateStore()({ model: incrementingModel, collection: growingCollection });
+            store = bbCreateStore()(reducer, { model: incrementingModel, collection: growingCollection });
             store.subscribe(() => listenerCalledCount++);
         });
 
@@ -82,163 +174,14 @@ describe('e2e communication', () => {
 
         describe('store -> backbone', () => {
             it('should affect model change via store.dispatch', () => {
-                store.dispatch({ type: 'DECREMENT' });
-                store.dispatch({ type: 'POP' });
-
-                assert.equal(incrementingModel.get('num'), 2);
-                assert.equal(growingCollection.length, 2);
-                assert.equal(store.getState().model.num, 2);
-                assert.equal(store.getState().collection.length, 2);
-                assert.equal(listenerCalledCount, 2);
-            });
-        });
-    });
-
-    describe('bbCreateStore(createStore)(model)', () => {
-        beforeEach(() => {
-            listenerCalledCount = 0;
-            incrementingModel = new IncrementingModel({ num: 3 });
-            store = bbCreateStore(createStore)(incrementingModel);
-            store.subscribe(() => listenerCalledCount++);
-        });
-
-        describe('backbone -> store', () => {
-            it('should affect store change via bbDispatch', () => {
-                bbDispatch(incrementingModel, 'INCREMENT');
-
-                assert.equal(incrementingModel.get('num'), 4);
-                assert.equal(store.getState().num, 4);
-                assert.equal(listenerCalledCount, 1);
-            });
-        });
-
-        describe('store -> backbone', () => {
-            it('should affect model change via store.dispatch', () => {
                 store.dispatch({ type: 'INCREMENT' });
+                store.dispatch({ type: 'PUSH', payload: {} });
 
                 assert.equal(incrementingModel.get('num'), 4);
-                assert.equal(store.getState().num, 4);
-                assert.equal(listenerCalledCount, 1);
-            });
-        });
-    });
-
-    describe('bbCreateStore()(reducer)', () => {
-        const collectionReducer = collection => (state, { type, payload }) => {
-            switch (type) {
-            case 'PUSH':
-                collection.push(payload);
-                return collection;
-            case 'POP':
-                collection.pop();
-                return collection;
-            default:
-                return collection;
-            }
-        };
-
-        const modelReducer = model => (state, { type, payload }) => {
-            switch (type) {
-            case 'INCREMENT':
-                model.set({ num: model.get('num') + 1 });
-                return model;
-            case 'DECREMENT':
-                model.set({ num: model.get('num') - 1 });
-                return model;
-            default:
-                return model;
-            }
-        };
-
-        beforeEach(() => {
-            listenerCalledCount = 0;
-            growingCollection = new Backbone.Collection([{}, {}, {}]);
-            incrementingModel = new Backbone.Model({ num: 3 });
-        });
-
-        describe('reducer returning collection', () => {
-            beforeEach(() => {
-                store = bbCreateStore()(collectionReducer(growingCollection));
-                store.subscribe(() => listenerCalledCount++);
-            });
-
-            it('throws if reducer or bbEntity not passed', () => {
-                assert.throws(
-                    () => bbCreateStore()(),
-                    'bbCreateStore()(bbEntityOrReducer) - Must give a bb entity or a reducer which returns one.'
-                );
-            });
-
-            it('throws if reducer returns undefined', () => {
-                assert.throws(
-                    () => bbCreateStore()(() => undefined),
-                    'Reducer must have a return value'
-                );
-            });
-
-            it('should not change state via bbDispatch()', () => {
-                bbDispatch(growingCollection, 'POP');
-                assert.equal(listenerCalledCount, 0);
-                assert.equal(growingCollection.length, 3);
-                assert.equal(store.getState().length, 3);
-            });
-
-            it('should change state via store.dispatch()', () => {
-                store.dispatch({ type: 'PUSH', payload: { new: true } });
-
-                assert.equal(growingCollection.last().get('new'), true);
-                assert.equal(listenerCalledCount, 1);
                 assert.equal(growingCollection.length, 4);
-                assert.equal(store.getState().length, 4);
-            });
-
-            it('should be called with the previous state in the next reducer call', () => {
-                const prevStateArgs = [];
-                const defaultState = { defaultStateArg: true };
-
-                store = bbCreateStore()((state, { type }) => {
-                    prevStateArgs.push(state);
-
-                    switch(type) {
-                    case 'INCREMENT':
-                        return 'incremented';
-                    case 'DECREMENT':
-                        return 'decremented';
-                    default:
-                        return state;
-                    }
-                }, defaultState);
-
-                store.dispatch({ type: 'INCREMENT' });
-                store.dispatch({ type: 'DECREMENT' });
-                store.dispatch({ type: 'Just push it' });
-
-                assert.deepEqual(prevStateArgs[0], defaultState);
-                assert.deepEqual(prevStateArgs[1], defaultState);
-                assert.equal(prevStateArgs[2], 'incremented');
-                assert.equal(prevStateArgs[3], 'decremented');
-            });
-        });
-
-        describe('multiple reducers with combineReducer', () => {
-            beforeEach(() => {
-                store = bbCreateStore()(combineReducers({
-                    collection: collectionReducer(growingCollection),
-                    model: modelReducer(incrementingModel),
-                }));
-                store.subscribe(() => listenerCalledCount++);
-            });
-
-            it('should change the collection and model state via store.dispatch()', () => {
-                store.dispatch({ type: 'PUSH', payload: { new: true } });
-                store.dispatch({ type: 'INCREMENT' });
-
-                assert.equal(growingCollection.last().get('new'), true);
-                assert.equal(incrementingModel.get('num'), 4);
-                assert.equal(listenerCalledCount, 2);
-                assert.equal(growingCollection.length, 4);
-                assert.equal(store.getState().collection.length, 4);
                 assert.equal(store.getState().model.num, 4);
+                assert.equal(store.getState().collection.length, 4);
+                assert.equal(listenerCalledCount, 2);
             });
         });
     });
